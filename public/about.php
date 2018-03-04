7187c926a1d2d62071efe7e9c8e8225dc3453a59
Ken <kennethbacayo@gmail.com>
2018-03-04 12:26:56 +0800

    home and about done.

--- public/about.php
+++ public/about.php
@@ -17,9 +17,50 @@
     
   <?php include '../includes/layouts/header.php';?>
 
+             <div class=" jumbotron container-fluid" style="padding-top: 100px; padding-bottom:100px;" >
+             <div class="container">
+                <div class="row">
+          <div class="col-sm-12">
+              <h1>Fixpertr</h1>
+            <h3>Fixpertr is an easy way to find services in the Philippines. Need something done? You can now find the right service 
+  providers or freelancers who    can help you. All you need to do is search, compare, choose and contact the providers! 
+  Are you a freelancer or a business offering services? You can now advertise to more targeted customers -- those seeking your services,
+  who are within your reach locally. In simple terms, Fixpert is an online classified ads just for services.
+  Because you want a more focused site with high quality ads, with less clutter and less spam. We should also mention that it's 100% FIlipino-made.</h3>
+          </div>
+        </div>
 
-      <div class="content container">
-         <h2>About</h2>
+             </div>
+            
+        </div>
+
+      <div class=" container">
+ 
+        
+
+        <div class="row">
+        <h2 class="text-center">Meet Our Team</h2>
+        
+          <div class="col-md-4" style="">
+            <img class="img-circle img-responsive" src="images/vien.jpg" style="width: 250px; ">
+            <h3 class="text-center">Vivien D. Torrizo</h3>
+            <h3 class="text-center"><span class="label label-success">Hustler</span></h3>
+          </div>
+
+          <div class="col-md-4">
+            <img class="img-circle img-responsive" src="images/normie.jpg" style="width: 250px; ">
+            <h3 class="text-center">Normie A. Cagandahan</h3>
+            <h3 class="text-center"><span class="label label-primary">Hispster</span></h3>
+          </div>
+
+          <div class="col-md-4">
+            <img class="img-circle img-responsive" src="images/ken.jpg" style="width: 250px; ;" >
+            <h3 class="text-center">Kenneth M. Bacayo</h3>
+            <h3 class="text-center"><span class="label label-danger">Hacker</span></h3>
+          </div>
+
+        </div>
+         
       </div>
    
   
--- public/index.php
+++ public/index.php
@@ -15,28 +15,115 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="stylesheets/mystyles.c ss">
+    <link rel="stylesheet" type="text/css" href="stylesheets/mystyles.css">
   </head>
   <body id="home">
     
   <?php include '../includes/layouts/header.php';?>
 
+   
+
+      <div class="content container-fluid bg" style="padding-top: 150px; padding-bottom: 150px;" >
     
+        
 
-      <div class="content container">
-         <h2>Home</h2>
-
-        <div class="row">
-          <div class="col-sm-4">
-            <?php if (isset($_GET['accountdeletion'])) { ?>
-            <div class="alert alert-success">Your account deleted successfully.</div> 
-
-          <?php } ?>
+         <div class="center">
+            <div class="row">
+              <div class="col-sm-8 col-sm-offset-2"><h1 style="color:white;"><strong>Fixpertr a web and mobile application for repair and alteration services</strong>  </h1></div>
+            </div>
+            
+             <a href="signup.php" class="btn btn-success btn-lg" role="button">Sign Up Now!</a>
           </div>
-        </div>
-         
+
+     
 
       </div>
+      <br>
+      <div class="container">
+        <div class="row">
+          <h2 class="text-center"><strong>Features</strong></h2>
+
+
+        <div class="row">
+       
+        
+          <div class="col-md-4" style="">
+            <table>
+              <tr>
+                <td style="padding: 15px;">
+                  <span class="glyphicon glyphicon-search features" style=" color:skyblue;"></span>  
+                </td>
+                <td colspan="2">
+                  <h3>Search repair and alteration shops</h3>
+                </td>
+              </tr>
+            </table>
+          
+         
+          </div>
+
+          <div class="col-md-4">
+          <table>
+              <tr>
+                <td style="padding: 15px;">
+                  <span class="glyphicon glyphicon-calendar features"></span>  
+                </td>
+                <td colspan="2">
+                  <h3>Set schedules to repair and alteration shops</h3>
+                </td>
+              </tr>
+            </table>
+          
+          </div>
+
+          <div class="col-md-4">
+                <table>
+              <tr>
+                <td style="padding: 15px;">
+                  <span class="glyphicon glyphicon-map-marker features" style="color:
+ #ff1a1a" ></span>  
+                </td>
+                <td colspan="2">
+                  <h3>You can see the shop locations</h3>
+                </td>
+              </tr>
+            </table>
+          
+          </div>
+          <div class="col-md-4">
+                <table>
+              <tr>
+                <td style="padding: 15px;">
+                  <span class="glyphicon glyphicon-cog features" style="color: #8c8c8c"></span>  
+                </td>
+                <td colspan="2">
+                  <h3>Manage your own shop as service provider</h3>
+                </td>
+              </tr>
+            </table>
+          
+          </div>
+
+            <div class="col-md-4">
+                <table>
+              <tr>
+                <td style="padding: 15px;">
+                  
+                    <img src="images/Paypal-icon.png" id="paypal"> 
+                </td>
+                <td colspan="2">
+                    <h3>Online payment</h3> 
+                </td>
+              </tr>
+
+            </table>
+          
+          </div>
+
+        </div>
+        </div>
+      </div>
+
 
 
 
